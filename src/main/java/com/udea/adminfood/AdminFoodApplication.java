4cd116fd2c5a2541a78540a56735673c91b6ff50
Jhon <jhond.b.v@gmail.com>
2021-08-25 21:00:03 -0500

    Init
    Initialization

---
+++ src/main/java/com/udea/adminfood/AdminFoodApplication.java
@@ -0,0 +1,13 @@
+package com.udea.adminfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/adminfood/AdminFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.adminfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
